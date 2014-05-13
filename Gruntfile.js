a20251a4dca84cf8135da122fd6552999d882544
kemar <marc@marcarea.com>
2014-05-13 09:48:26 +0200

    Added the grunt jshint task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,14 @@
-  'use strict';
+'use strict';
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    meta: {
-      banner: [
+    // Project configuration.
+    grunt.initConfig({
+
+        pkg: grunt.file.readJSON('package.json'),
+
+        meta: {
+            banner: [
                 '/**',
                 ' * <%= pkg.description %>',
                 ' * @version v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>' +
@@ -14,166 +16,178 @@ module.exports = function(grunt) {
                 ' * @author <%= pkg.author %>',
                 ' * @license MIT License, http://www.opensource.org/licenses/MIT',
                 ' */\n'
-              ].join('\n')
-    },
-    dirs: {
-      dest: 'dist'
-    },
-    clean: [ 
-      '<%= dirs.dest %>'
-    ],
-    concat: {
-      options: {
-        banner: '<%= meta.banner %>'
-      },
-      dist: {
-        src: ['src/*.js'],
-        dest: '<%= dirs.dest %>/<%= pkg.name %>.js'
-      }
-    },
-    recess: {
-      build: {
-        src: [ 'src/angular-wizard.less' ],
-        dest: '<%= dirs.dest %>/<%= pkg.name %>.css',
-        options: {
-          compile: true,
-          compress: false,
-          noUnderscores: false,
-          noIDs: false,
-          zeroUnits: false
-        }
-      },
-      compile: {
-        src: [ 'src/angular-wizard.less' ],
-        dest: '<%= dirs.dest %>/<%= pkg.name %>.min.css',
-        options: {
-          compile: true,
-          compress: true,
-          noUnderscores: false,
-          noIDs: false,
-          zeroUnits: false
-        }
-      }
-    },
-    copy: {
-      less_files: {
-        files: [
-          { 
-            src: [ 'src/angular-wizard.less' ],
-            dest: '<%= dirs.dest %>',
-            cwd: '.',
-            expand: true,
-            flatten: true
-          }
-       ]   
-      }
-    },
-    bowerInstall: {
-        install: {
-        }
-    },
-    html2js: {
-      angularwizard: {
-        options: {
-          base: 'src'
+            ].join('\n')
         },
-        src: [ 'src/*.html' ],
-        dest: 'src/<%= pkg.name %>.tpls.js'
-      },
-    },
-    uglify: {
-      options: {
-        banner: '<%= meta.banner %>'
-      },
-      dist: {
-        src: ['<%= concat.dist.dest %>'],
-        dest: '<%= dirs.dest %>/<%= pkg.name %>.min.js'
-      }
-    },
-    karma: {
-      options: {
-        configFile: 'karma.conf.js'
-      },
-      build: {
-        singleRun: true,
-        autoWatch: false
-      },
-      travis: {
-        singleRun: true,
-        autoWatch: false,
-        browsers: ['Firefox']
-      },
-      dev: {
-        autoWatch: true
-      }
-    },
-    changelog: {
-      options: {
-        dest: 'CHANGELOG.md'
-      }
-    }
-  });
 
-  // Load the plugin that provides the "concat" task.
-  grunt.loadNpmTasks('grunt-contrib-concat');
+        dirs: {
+            dest: 'dist'
+        },
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+        clean: [
+            '<%= dirs.dest %>'
+        ],
 
-  grunt.loadNpmTasks('grunt-bower-task');
+        // Validate files with JSHint.
+        // https://github.com/gruntjs/grunt-contrib-jshint
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc'
+            },
+            all: [
+                'src/*.js',
+                'test/*.js',
+                'Gruntfile.js'
+            ]
+        },
 
-  grunt.renameTask("bower", "bowerInstall");
+        concat: {
+            options: {
+                banner: '<%= meta.banner %>'
+            },
+            dist: {
+                src: ['src/*.js'],
+                dest: '<%= dirs.dest %>/<%= pkg.name %>.js'
+            }
+        },
 
-  grunt.loadNpmTasks('grunt-karma');
-  grunt.loadNpmTasks('grunt-karma');
+        recess: {
+            build: {
+                src: ['src/angular-wizard.less'],
+                dest: '<%= dirs.dest %>/<%= pkg.name %>.css',
+                options: {
+                    compile: true,
+                    compress: false,
+                    noUnderscores: false,
+                    noIDs: false,
+                    zeroUnits: false
+                }
+            },
+            compile: {
+                src: ['src/angular-wizard.less'],
+                dest: '<%= dirs.dest %>/<%= pkg.name %>.min.css',
+                options: {
+                    compile: true,
+                    compress: true,
+                    noUnderscores: false,
+                    noIDs: false,
+                    zeroUnits: false
+                }
+            }
+        },
 
-  grunt.loadNpmTasks('grunt-conventional-changelog');
+        copy: {
+            less_files: {
+                files: [{
+                    src: ['src/angular-wizard.less'],
+                    dest: '<%= dirs.dest %>',
+                    cwd: '.',
+                    expand: true,
+                    flatten: true
+                }]
+            }
+        },
 
-  grunt.loadNpmTasks('grunt-recess');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-ngmin');
-  grunt.loadNpmTasks('grunt-html2js');
+        html2js: {
+            angularwizard: {
+                options: {
+                    base: 'src'
+                },
+                src: ['src/*.html'],
+                dest: 'src/<%= pkg.name %>.tpls.js'
+            },
+        },
 
+        uglify: {
+            options: {
+                banner: '<%= meta.banner %>'
+            },
+            dist: {
+                src: ['<%= concat.dist.dest %>'],
+                dest: '<%= dirs.dest %>/<%= pkg.name %>.min.js'
+            }
+        },
 
-  // Default task.
-  grunt.registerTask('default', ['build']);
+        karma: {
+            options: {
+                configFile: 'karma.conf.js'
+            },
+            build: {
+                singleRun: true,
+                autoWatch: false
+            },
+            travis: {
+                singleRun: true,
+                autoWatch: false,
+                browsers: ['Firefox']
+            },
+            dev: {
+                autoWatch: true
+            }
+        },
 
-  // Build task.
-  grunt.registerTask('build', [
-    'clean',
-    'bowerInstall', 
-    'copy',
-    'recess',
-    'html2js',
-    'concat', 
-    'uglify', 
-    'karma:build']);
+        changelog: {
+            options: {
+                dest: 'CHANGELOG.md'
+            }
+        }
 
-  grunt.registerTask('test', ['build']);
-  
-  grunt.registerTask('travis', ['build']);
+    });
 
-  // Provides the "bump" task.
-  grunt.registerTask('bump', 'Increment version number', function() {
-    var versionType = grunt.option('type');
-    function bumpVersion(version, versionType) {
-      var type = {patch: 2, minor: 1, major: 0},
-          parts = version.split('.'),
-          idx = type[versionType || 'patch'];
-      parts[idx] = parseInt(parts[idx], 10) + 1;
-      while(++idx < parts.length) { parts[idx] = 0; }
-      return parts.join('.');
-    }
-    var version;
-    function updateFile(file) {
-      var json = grunt.file.readJSON(file);
-      version = json.version = bumpVersion(json.version, versionType || 'patch');
-      grunt.file.write(file, JSON.stringify(json, null, '  '));
-    }
-    updateFile('package.json');
-    updateFile('bower.json');
-    grunt.log.ok('Version bumped to ' + version);
-  });
+    grunt.loadNpmTasks('grunt-bower-task');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-conventional-changelog');
+    grunt.loadNpmTasks('grunt-html2js');
+    grunt.loadNpmTasks('grunt-karma');
+    grunt.loadNpmTasks('grunt-ngmin');
+    grunt.loadNpmTasks('grunt-recess');
+
+    grunt.registerTask('default', ['build']);
+
+    grunt.registerTask('build', [
+        'clean',
+        'copy',
+        'recess',
+        'html2js',
+        'concat',
+        'uglify',
+        'karma:build'
+    ]);
+
+    grunt.registerTask('test', ['build']);
+
+    grunt.registerTask('travis', ['build']);
+
+    grunt.registerTask('bump', 'Increment version number', function () {
+        var versionType = grunt.option('type');
+
+        function bumpVersion(version, versionType) {
+            var type = {
+                patch: 2,
+                minor: 1,
+                major: 0
+            },
+                parts = version.split('.'),
+                idx = type[versionType || 'patch'];
+            parts[idx] = parseInt(parts[idx], 10) + 1;
+            while (++idx < parts.length) {
+                parts[idx] = 0;
+            }
+            return parts.join('.');
+        }
+        var version;
+
+        function updateFile(file) {
+            var json = grunt.file.readJSON(file);
+            version = json.version = bumpVersion(json.version, versionType || 'patch');
+            grunt.file.write(file, JSON.stringify(json, null, '  '));
+        }
+        updateFile('package.json');
+        updateFile('bower.json');
+        grunt.log.ok('Version bumped to ' + version);
+    });
 
 };
